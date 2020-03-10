d730fc8ec7d5c04a2f0dd0e5fdcec402b5036d0d
goforitrj <eleveniasjr@gmail.com>
2020-03-10 16:31:55 +1100

    Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
@@ -15,17 +15,20 @@ const App = () => {
                     <Link to="/about">About</Link>
                 </li>
             </ul>
-            <Route path="/" exact={true} component={Home}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route
-                path="/profiles/:username"
-                exact={true}
-                component={Profiles}
-            ></Route>
-            <Route
-                path="/profiles/:username/:country"
-                component={Profiles}
-            ></Route>
+            <Switch>
+                <Route path="/" exact={true} component={Home}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route
+                    path="/profiles/:username"
+                    exact={true}
+                    component={Profiles}
+                ></Route>
+                <Route
+                    path="/profiles/:username/:country"
+                    component={Profiles}
+                ></Route>
+                <Route render={({location}) => <div><h2>{location.pathname} is not exist</h2></div>} />
+            </Switch>
         </div>
     );
 };
